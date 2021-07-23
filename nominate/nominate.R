6d7671914d7d1cb85eedd1410fb8dae4cfa7725b
Jan Dimter S <jandimter@gmail.com>
2021-07-23 12:13:55 -0400

    Update nominate.R

--- nominate/nominate.R
+++ nominate/nominate.R
@@ -1,3 +1,8 @@
+
+# Carga de librerías requeridas
+library(coda)
+library(purrr)
+library(dplyr)
 library(tidyverse)
 library(rvest)
 library(tidytext)
@@ -7,18 +12,20 @@ library(readxl)
 library(wnominate)
 library(wnomadds)
 
-
+# Carga objetos con votaciones
 votos_cc <- list.files(pattern = ".rds") %>%
   map(readRDS) %>% 
   bind_rows()
 
 
-
+# Agrega id de votación por fila 
 votos_cc$id_votacion <- paste0("bol_",votos_cc$id_votacion)
 
+# Transforma a columnas IDs de votación
 votos_cc <- votos_cc %>%
   pivot_wider(names_from = id_votacion, values_from = tipo_voto)
 
+# Recodificación votación
 votos_cc <- as.data.frame(votos_cc)
 votos_cc[votos_cc=="NULL"] = 'NA'
 votos_cc[votos_cc=="A Favor"] <- "1"
@@ -28,19 +35,17 @@ votos_cc[votos_cc=="Abstencion"] <- "9"
 
 
 
-# aqui antes transcribimos las votaciones de declaracion sobre los presos y todas las votaciones de eleccion de la mesa de la CC
+# Carga de votaciones que no están aún en sistema online. Transcripción manual.
 votos_cc_first_part <- read_delim("https://storage.googleapis.com/notas-blog-public/nominate/votos_cc_first_part.csv", 
                                   ";", escape_double = FALSE, locale = locale(encoding = "latin1"), 
                                   trim_ws = TRUE)
-names(votos_cc_first_part)
 
+# Join dfs
 votos_cc <- votos_cc %>% 
   left_join(votos_cc_first_part %>% select(-c(glosa_cand)),by="nombres_cc")
 
 
-##
-
-
+# Asignación nombre filas
 votos_cc <-  votos_cc %>%
   column_to_rownames("nombres_cc")
 
@@ -52,7 +57,7 @@ parlamentarios <- data.frame(parlamentario=rownames(votos_cc))
 legData_party_bol <- votos_cc %>%
   select(coalicion) %>%
   rename(coalicion=coalicion)
-colnames(legData_party_bol) <- 'party'
+
 
 
 
@@ -69,54 +74,52 @@ rc_party_bol <- rollcall(data = votos_cc,
                          legis.data = legData_party_bol,
                          yea = 1, nay = 6, missing = 9, notInLegis = NA)
 
-rc_party_bol
 
+##########################
+# W-Nominate - Plots
+##########################
 
+# 1D usando wmominate #Omite dos casos (Squella y Harboe)
 
-ideal_1 <- ideal(rc_party_bol, d=1) # ideal point
-ideal_1
+wmoni_party_bol <- wnominate(rc_party_bol, polarity = 106,dims=1,trials = 200)
 
+# Reconstruccion usando datos paquete wnominate
 
+df_graph <- data.frame(name=rownames(wmoni_party_bol$legislators),coalicion=wmoni_party_bol$legislators$coalicion,coord=wmoni_party_bol$legislators$coord1D,se=wmoni_party_bol$legislators$se1D)
 
+# Crea funciones mean y sd y las agrega como columnas
 
-plot(ideal_1)
+mean_valid<-function(x){round(mean(na.omit(x)),3)}
+sd_valid<-function(x){round(sd(x,na.rm=TRUE),3)}
+df_graph$mean_party<-ave(df_graph$coord,df_graph$coalicion,FUN=mean_valid)
+df_graph$sd_party<-ave(df_graph$coord,df_graph$coalicion,FUN=sd_valid)
 
+# Crea columna resumen
 
+for (i in 1:length(df_graph$name)){df_graph$gral[i]<-paste0(df_graph$coalicion[i],'\n Prom:',df_graph$mean[i],' (',df_graph$sd[i],')')}
 
-pp <- data.frame(ideal_1$xbar)
+# Plot W-Nominate (individual) 1 Dimensión
 
-
-
-legData_party_bol <- legData_party_bol %>%
-  rownames_to_column(var="name_voter")
-
-pp <- pp %>%
-  rownames_to_column(var="name_voter")
-
-pp <- pp %>% 
-  left_join(legData_party_bol, by="name_voter")
-
-p <-pp %>%
-  ggplot(aes(reorder(name_voter,D1), D1, label=name_voter)) +
-  geom_point(stat='identity', aes(col=party), size=2.5) + scale_color_viridis_d() + coord_flip()
+p<-df_graph %>%
+  ggplot(aes(reorder(name,coord), coord, label=name)) +
+  xlab("Nombre") + ylab("Puntaje unidimensional")+
+  geom_point(stat='identity', aes(col=coalicion), size=2.5) + scale_color_viridis_d() + coord_flip()+theme_bw()+ theme(legend.position="top")+theme(legend.position = c(0.8, 0.4))+geom_errorbar(aes(ymin=coord-se, ymax=coord+se), width=.2,position=position_dodge(0.05))
 p
 
-ggsave(plot = p, "ideal_point_lec.png", width = 22, height = 25)
+# Plot W-Nominate por coalicion 1 Dimensión
+
+p <-df_graph%>%
+  ggplot(aes(reorder(gral,mean_party), coord, label=name)) +
+  xlab("Pacto") + ylab("Puntaje unidimensional")+
+  geom_point(stat='identity', aes(col=coalicion), size=2,alpha = 0.4,show.legend = FALSE) + scale_color_viridis_d() + coord_flip()+ theme_bw()
+p
 
 
-##########################
-##########################
-# 2D
+# Plot W-Nominate 2 Dimensiones
 
-
-wmoni_party_bol <- wnominate(rc_party_bol, polarity = c(106,66))
-wmoni_party_bol
-plot.coords(wmoni_party_bol)
-plot.coords(wmoni_party_bol, legend.x = 1, legend.y = 1, plotBy = "party",
-            main.title = "W-NOMINATE", d1.title = "Primera Dimensi?n",
-            d2.title = "Segunda Dimensi?n")
-
-
-
-
-
+wmoni_party_bol_2d <- wnominate(rc_party_bol, polarity = c(106,66),trials=200)
+wmoni_party_bol_2d
+plot.coords(wmoni_party_bol_2d)
+plot.coords(wmoni_party_bol_2d, legend.x = 1, legend.y = 1, plotBy = "coalicion",
+            main.title = "W-NOMINATE", d1.title = "Primera Dimensión",
+            d2.title = "Segunda Dimensión")
